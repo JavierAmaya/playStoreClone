5d076f40774a483ecddc619c4ca5e9be90cd931a
JavierAmaya <photojavhn@gmail.com>
2021-06-09 12:52:48 -0600

    adding functions with js

---
+++ js/controlador.js
@@ -0,0 +1,135 @@
+//Codigo para generar información de categorias y almacenarlas en un arreglo.
+var categorias = [];
+(()=>{
+  //Este arreglo es para generar textos de prueba
+  let textosDePrueba=[
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore, modi!",
+      "Quos numquam neque animi ex facilis nesciunt enim id molestiae.",
+      "Quaerat quod qui molestiae sequi, sint aliquam omnis quos voluptas?",
+      "Non impedit illum eligendi voluptas. Delectus nisi neque aspernatur asperiores.",
+      "Ducimus, repellendus voluptate quo veritatis tempora recusandae dolorem optio illum."
+  ]
+  
+  //Genera dinamicamente los JSON de prueba para esta evaluacion,
+  //Primer ciclo para las categorias y segundo ciclo para las apps de cada categoria
+
+  
+  let contador = 1;
+  for (let i=0;i<5;i++){//Generar 5 categorias
+      let categoria = {
+          nombreCategoria:"Categoria "+i,
+          descripcion:textosDePrueba[Math.floor(Math.random() * (5 - 1))],
+          aplicaciones:[]
+      };
+      for (let j=0;j<10;j++){//Generar 10 apps por categoria
+          let aplicacion = {
+              codigo:contador,
+              nombre:"App "+contador,
+              descripcion:textosDePrueba[Math.floor(Math.random() * (5 - 1))],
+              icono:`img/app-icons/${contador}.webp`,
+              instalada:contador%3==0?true:false,
+              app:"app/demo.apk",
+              calificacion:Math.floor(Math.random() * (5 - 1)) + 1,
+              descargas:1000,
+              desarrollador:`Desarrollador ${(i+1)*(j+1)}`,
+              imagenes:["img/app-screenshots/1.webp","img/app-screenshots/2.webp","img/app-screenshots/3.webp"],
+              comentarios:[
+                  {comentario:textosDePrueba[Math.floor(Math.random() * (5 - 1))],calificacion:Math.floor(Math.random() * (5 - 1)) + 1,fecha:"12/12/2012",usuario:"Juan"},
+                  {comentario:textosDePrueba[Math.floor(Math.random() * (5 - 1))],calificacion:Math.floor(Math.random() * (5 - 1)) + 1,fecha:"12/12/2012",usuario:"Pedro"},
+                  {comentario:textosDePrueba[Math.floor(Math.random() * (5 - 1))],calificacion:Math.floor(Math.random() * (5 - 1)) + 1,fecha:"12/12/2012",usuario:"Maria"},
+              ]
+          };
+          contador++;
+          categoria.aplicaciones.push(aplicacion);
+      }
+      categorias.push(categoria);
+  }
+  
+  console.log(categorias);
+})();
+
+function generarAplicaciones() {
+    let estrellas = '';
+    
+    categorias.forEach(app => {
+       app.aplicaciones.forEach(element => {
+        
+        estrellas = '';
+        for (let i = 0; i < element.calificacion; i++) {
+            estrellas +='<i class="fas fa-star"></i>'
+        }
+
+        for (let i = 0; i < (5-element.calificacion); i++) {
+            estrellas +='<i class="far fa-star"></i>'
+        }
+          document.getElementById('aplicaciones').innerHTML += 
+                    `<div class="col-xl-2 col-md-3 col-sm-6 margin-cards">
+                        <div class="card" onClick="mostrarModal()";>
+                            <img class="card-img-top img-card" src="${element.icono}" alt="Card image cap">
+                            <div class="card-body">
+                            <h5 class="card-title">${element.nombre}</h5>
+                            <p class="card-text">${element.desarrollador}</p>
+                            <p class="card-text">
+                                ${estrellas}
+                            </p>
+                            <p class="card-text"><small class="text-muted"><strong>$45</strong></small></p>
+                            </div>
+                        </div>
+                </div>`
+            ;
+       });
+    });
+}
+
+
+function selectImagenes() {
+    for (let i = 0; i < 50; i++) {
+        document.getElementById('img').innerHTML += `
+                <option value="img/app-icons/${i}.webp">Imagen ${i}</option>
+        `;
+        
+    }
+}
+selectImagenes();
+
+
+function generarCategorias() {
+    let valCat = 0;
+    categorias.forEach(element => {
+        document.getElementById('select-cate').innerHTML+=`
+            <option value="${valCat+1}">${element.nombreCategoria}</option>
+
+        `;
+    });
+    
+}
+generarCategorias();
+generarAplicaciones();
+
+function mostrarModal() {
+    $('#modalApp').modal('show');
+}
+
+
+
+function guardar() {
+
+    const app = {
+        app: "",
+        calificacion:  document.getElementById('select-calificacion').value,
+        codigo: "",
+        comentarios:  [],
+        desarrollador:  document.getElementById('nameDevelopment').value,
+        descargas: "",
+        descripcion: "",
+        icono: document.getElementById('img').value,
+        imagenes: "",
+        instalada: "",
+        nombre: document.getElementById('nameApp').value
+    }
+
+    console.log(app);
+
+    // aplicaciones.push(app);
+    
+}
